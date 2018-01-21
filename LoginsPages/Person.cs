5b1f8da6168921abbc7ec28538eb0b6496b10f1e
MaximkaStar <max.martsinkus@mail.ru>
2018-01-21 21:44:17 +0600

    ..

---
+++ LoginsPages/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginsPages
+{
+    [Serializable]
+    public class Person
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PersonPathImageFile { get; set; }
+        public string OtherTxt { get; set; }
+        public Person()
+        {
+            OtherTxt = "Empty";
+        }
+    }
+}
